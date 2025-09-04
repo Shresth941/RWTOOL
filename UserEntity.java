02c5c3202af8de1ca890ec8eaf18bec4c626d504
Shresth Singh <118938064+Shresth941@users.noreply.github.com>
2025-09-04 15:34:52 +0530

    Create UserEntity.java

---
+++ UserEntity.java
@@ -0,0 +1,22 @@
+package RwTool.rwtool.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "full_name", nullable = false)
+    private String fullname;
+
+    // Add other fields from your team's entity...
+}
