800156bb21ba6322c0deb26e219e7437447002e4
Shresth Singh <118938064+Shresth941@users.noreply.github.com>
2025-09-04 15:36:18 +0530

    Create UserAlreadyExistException.java

---
+++ UserAlreadyExistException.java
@@ -0,0 +1,7 @@
+package RwTool.rwtool.exception;
+
+public class UserAlreadyExistException extends RuntimeException {
+    public UserAlreadyExistException(String message) {
+        super(message);
+    }
+}
